707e4c3d56f2b3f6a959cff060cb0a9fa62be23e
Daniel Righetti <danrighetti27@gmail.com>
2022-08-18 18:25:43 -0300

    Exercícios Feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,38 @@
 const array = [
-    [27, 4, 20, 13, 14],
-    [11, 15, 12, 8, 9],
-    [5, 5, 12, 16, 4],
-    [20, 33, 11, 12, 19],
-    [3, 3, 4, 5, 10]
+    [27, 4, 20, 13, 14], //Índice 0 - Jogador 1
+    [11, 15, 12, 8, 9],  //Índice 1 - Jogador 2
+    [5, 5, 12, 16, 4],   //Índice 2- Jogador 3
+    [20, 33, 11, 12, 19],  //Índice 3 - Jogador 4
+    [3, 3, 4, 5, 10]      //Índice 4 - Jogador 5
 ]
+
+
+for(i = 0; i <= 4; i++){
+
+    console.log(`Jogador ${i + 1} : ${array[i]}`)
+}
+
+
+
+
+
+
+
+
+
+
+// outra resolução
+
+// if(array.length === 5){
+//     for(let i in array){
+//         let jogador
+//         jogador = `Jogador ${Number(i)+1}: `
+//         for(let j of array[i]){
+//             jogador += ` ${j} `
+//         }
+//         console.log(jogador)
+//     }
+// }else{
+//     console.log("é necessário atualizar os dados do elenco")
+// }
+
---
+++ exercicio3/script.js
@@ -0,0 +1,18 @@
+const timesDeSaoPaulo = ["Palmeiras", "Bragantino" , "São Paulo" , "Corinthians" , "Santos"];
+
+// const time1 = timesDeSaoPaulo[0]
+// const time2 = timesDeSaoPaulo[1]
+// const time3 = timesDeSaoPaulo[2]
+// const time4 = timesDeSaoPaulo[3]
+// const time5 = timesDeSaoPaulo[4]
+
+
+
+for(i of timesDeSaoPaulo){
+    // console.log(`1 - ${timesDeSaoPaulo[0] \n}`)
+
+    console.log(` ${i}`)
+
+}
+
+
